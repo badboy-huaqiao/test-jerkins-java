08cd65bcf1aceff5e0548a677b22db06bfa53421
Huaqiao Zhang <huaqiaoz@vmware.com>
2018-08-20 18:29:43 +0800

    Feat: add test module
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

---
+++ src/test/java/com/example/demo/ControllerTest.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.controller.PingController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RunWith(SpringRunner.class)
+//@SpringBootTest
+@WebMvcTest
+public class ControllerTest {
+	 @Autowired
+	 private MockMvc mockMvc;
+    
+//    @Autowired
+//    ObjectMapper objectMapper;
+	
+//	@Autowired
+//	PingController pingController;
+//    
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/api/v1/ping"))
+        			.andDo(print())
+        			.andExpect(status().isOk())
+        			.andExpect(content().string(containsString("pong")));
+    }
+//	@Test
+//    public void contexLoads() throws Exception {
+//        assertThat(pingController).isNotNull();
+//    }
+}
